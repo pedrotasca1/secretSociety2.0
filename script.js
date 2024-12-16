df8ad2e37c18b763cbffc1fcdd5a456fb04a98aa
Pedro Tasca <pedrotasca@Pedros-MacBook-Pro.local>
2024-12-16 13:42:57 +0000

    implemented multiple pins and styled section 4 and 5

--- script.js
+++ script.js
@@ -37,6 +37,17 @@ document.addEventListener("scroll", () => {
     canCollage.style.transform = `translate(${offsetX}px, ${offsetY}px) rotate(${rotation}deg)`;
 });
 
+const verticalScroll = gsap.utils.toArray(".vertical-scroll");
+
+gsap.to(verticalScroll, {
+    xPercent: -100 * (verticalScroll.length - 1),
+    scrollTrigger: {
+        trigger: "#section02",
+        pin: true,
+        scrub: 1,
+    },
+})
+
 // Horizontal Scroll GSAP
 const sectionHorizontal = gsap.utils.toArray(".section-content-container");
 const totalScrollWidth = (sectionHorizontal.length - 1) * 100;
@@ -57,5 +68,33 @@ gsap.to(sectionHorizontal, {
         },
         anticipatePin: 1,
         markers: true,
+    },
+});
+
+const lines = gsap.utils.toArray(".line");
+
+gsap.to(lines, {
+    opacity: 1, // Make each line visible
+    y: 0, // Move the text to its original position (from off-screen)
+    stagger: 1, // Delay between each line animation
+    scrollTrigger: {
+        trigger: "#section04", // Trigger when #section04 comes into view
+        pin: true, // Pin the section during scroll
+        scrub: 1, // Smooth scrolling effect
+        start: "bottom bottom", // Start when section04 enters the viewport
+        end: "+=800%", // Extend the scroll trigger to fit all lines
+    },
+});
+
+const sectionFive = document.querySelector(".horizontal-scroll-wrapper"); // Container to move horizontally
+
+gsap.to(sectionFive, {
+    xPercent: -100 * (sectionFive.children.length - 1), // Move based on the number of sections
+    scrollTrigger: {
+        trigger: "#section05", // Trigger when section05 comes into view
+        pin: true, // Pin the #section05 while the animation happens
+        scrub: 1, // Smooth scrolling effect
+        start: "top top", // Start when the top of section05 hits the top of the viewport
+        end: "+=1440%", // End when the entire horizontal scroll is completed
     },
 });
